8615adeda2d9b7bc4a3fe3e8f06875ebb7a90f1c
viktorolof <95023450+viktorolof@users.noreply.github.com>
2022-08-15 14:37:05 +0200

    Update ZebrOcelot.py

--- python/ZebrOcelot.py
+++ python/ZebrOcelot.py
@@ -1,18 +1,7 @@
 x = int(input())
-animals = [] #[highest -> lowest]
-lowest_o = -1
-for i in range(x):
-    str = input()
-    animals.append(str)
-    if(str == 'o'):
-        lowest = i
-
-iterations = 0
-ocelots = True
-if(lowest_o == -1):
-    print(0)
-else:
-    while(ocelots):
-        # kolla lowest ocelot, flippa till zebra, alla zebror under den, gör till ocelot, sätt nya lowest
-        
-        iterations += 1
+res = 0
+for i in range(x -1, -1, -1):
+    c = input()
+    if(c == 'O'):
+        res += pow(2, i)
+print(res)
